896be164a2116f5f71b2040777e872d50e28080e
tigrine sacha <sacha.tigrine@etu.umontpellier.fr>
2019-02-05 17:59:26 +0100

    Tp2 -- Exercice 1

--- src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
+++ src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
@@ -3,10 +3,23 @@ package fr.umontpellier.iut.exercice1;
 public class FizzBuzz {
 
     public String getValue(int i) {
-        throw new RuntimeException("Not yet implemented !");
+            if(i%3==0){
+                if(i%5==0){
+                    return "FizzBuzz";
+                }
+                return "Fizz";
+            }else if(i%5==0){
+                return "Buzz";
+            }else {
+                return ""+i;
+            }
     }
 
-    public String[] computeList(int i) {
-        throw new RuntimeException("Not yet implemented !");
+    public String[] computeList(int n) {
+        String[] chaine =new String[n];
+        for(int i=1;i<=n;i++){
+            chaine[i-1] += getValue(i);
+        }
+        return chaine;
     }
 }
